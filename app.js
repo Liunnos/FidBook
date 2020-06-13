2f9391bff0f4ab56ee1cc9eb6fa244fdb078da8e
Benoit Weil <liunnos@gmail.com>
2020-06-13 15:57:59 +0200

    implémentation CORS

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const xss = require('xss-clean');
 const hpp = require('hpp');
 const cookieParser = require('cookie-parser');
 const compression = require('compression');
+const cors = require('cors');
 
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controller/errorController');
@@ -25,6 +26,10 @@ app.set('views', path.join(__dirname, 'views'));
 
 // Middleware
 //1 middleware globaux
+// implementation cors
+app.use(cors());
+
+app.options('*', cors());
 // fichier statique
 app.use(express.static(path.join(__dirname, 'public')));
 
